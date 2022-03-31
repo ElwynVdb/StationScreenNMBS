cfdab823d159f2e774feb4b99bf1bc14d7608a1b
Elwyn Van der Borght <elwyn.vdb@gmail.com>
2022-03-31 22:28:56 +0200

    Train Board Working

--- src/AppRouter.ts
+++ src/AppRouter.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from "express";
+import { RailApi } from "./RailApi";
 const router = express.Router();
 
-router.get("/", (req: Request, res: Response) => {
-    res.status(200).render("index");
-}); 
+const station_name = "Antwerpen";
+
+router.get("/", async (req: Request, res: Response) => {
+    let data = (await RailApi.getLiveBoard(station_name, "nl", false))?.departures.departure.filter(d => parseInt(d.id) <= 28);
+    res.status(200).render("index", { station: station_name, departures: data });
+});
 
 module.exports = router; 
